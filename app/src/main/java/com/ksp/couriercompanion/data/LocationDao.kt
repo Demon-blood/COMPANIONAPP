adf6be2826293e703b55fb5140f70f8f151e4543
Demon-blood <64545469+Demon-blood@users.noreply.github.com>
2026-05-24 22:48:09 +0200

    cleansheet

--- app/src/main/java/com/ksp/couriercompanion/data/LocationDao.kt
+++
@@ -1,8 +0,0 @@
-package com.ksp.couriercompanion.data
-import androidx.room.*
-
-@Dao
-interface LocationDao {
-    @Insert suspend fun insert(ping: LocationPingEntity): Long
-    @Query("SELECT * FROM location_pings ORDER BY timestamp DESC LIMIT 1") suspend fun last(): LocationPingEntity?
-}
